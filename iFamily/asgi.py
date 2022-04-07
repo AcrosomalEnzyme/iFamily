2c61bece201d6b85435550f64d87f86bf00ef2d6
lyl_pycharm <1209532122@qq.com>
2022-04-07 22:25:45 +0800

    change project2

--- iFamily/asgi.py
+++ iFamily/asgi.py
@@ -6,11 +6,26 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
 import os
-
-from django.core.asgi import get_asgi_application
-
+#引入环境变量
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iFamily.settings')
+#必须先执行
+django.setup()
 
-application = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from game.routing import websocket_urlpatterns
+
+
+#实现在channels外调用channels的函数
+from channels.layers import get_channel_layer
+channel_layer = get_channel_layer()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+})
+
