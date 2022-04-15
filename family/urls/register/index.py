02f13b372e9888dbef0577b071d97d48d46f747f
lyl_pycharm <1209532122@qq.com>
2022-04-15 11:23:22 +0800

    fix family register page view

--- family/urls/register/index.py
+++ family/urls/register/index.py
@@ -5,7 +5,7 @@ from family.views.register.register_index import register_index
 # 之后每个文件夹都要创建index.py，内容除了没有include其他都一样
 urlpatterns = {
     # 第一个是直接添加主页的url，信息已经足够，不需要定位到具体文件夹继续往下查找
-    path("", register_index(), name="register_index"),
+    path("", register_index, name="register_index"),
     # path("menu/", include("game.urls.menu.index")),
     # path("playground/", include("game.urls.playground.index")),
     # path("settings/", include("game.urls.settings.index")),
